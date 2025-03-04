e9a8dcb5db371c40b6562839ad4d18627f091989
LucasCostaMrq <lucascostamarques816@gmail.com>
2025-03-04 18:28:49 -0300

    first commit of ToDo_List project

---
+++ configURL.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGO_URI:'mongodb+srv://lucasmarques:kvKLd8iF5Mox6gOL@apitodo.6sadu.mongodb.net/?retryWrites=true&w=majority&appName=apitodo'
+}
---
+++ model/task.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const TaksSchema = new mongoose.Schema({
+    tarefa:{
+        type: String,
+        required: true
+    },
+    descricao:{
+        type: String,
+        required: true
+    },
+    inserido:{
+        type: Date,
+        default: Date.now
+    },
+    prazo:{
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('tarefa', TaksSchema);
